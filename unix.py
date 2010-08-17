83d75366d5a52d9970bfe3433db9a221d4568b7e
Konstantin Yegupov <yk4ever@gmail.com>
2010-08-17 23:11:37 +0100

    Removed improper comment from unix.py

--- unix.py
+++ unix.py
@@ -20,7 +20,7 @@
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-# Low level interface - see UnRARDLL\UNRARDLL.TXT
+# Unix version uses unrar command line executable
 
 import subprocess
 import gc
