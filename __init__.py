562e7b22be5f188a49aee93b4594789a6ea0ada3
Konstantin Yegupov <yk4ever@gmail.com>
2010-08-19 10:24:44 +0100

    Version 0.99.1

--- __init__.py
+++ __init__.py
@@ -33,7 +33,7 @@ similar to the C interface provided by UnRAR. There is also a
 higher level interface which makes some common operations easier.
 """
 
-__version__ = '0.99'
+__version__ = '0.99.1'
 
 try:
     WindowsError
